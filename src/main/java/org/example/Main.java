22fcfc8dd9fe7371b56e663d285ce250d56a3864
dusman <dusman.nalin@gmail.com>
2025-03-30 00:41:19 +0530

    finalMetrix 2D is returned.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -31,7 +31,6 @@ public class Main {
             System.out.println("File not found!");
             return;
         }
-
         try {
 
             // Create ObjectMapper for reading JSON
@@ -39,20 +38,15 @@ public class Main {
 
             // Read the JSON into the root object (RootObject is the class representing the JSON structure)
             RootObject rootObject = objectMapper.readValue(inputStream, RootObject.class);
-
-            // Access and print some fields to verify it worked
-//            System.out.println("Columns: " + rootObject.getColumns());
-//            System.out.println("Rows: " + rootObject.getRows());
-
             List<StandardSymbol> standardSymbolList = rootObject.getProbabilities().getStandardSymbols();
 
+            String[][] standardSymbolMetrix = new String[3][3];
             String[][] finalMetrix = new String[3][3];
+
 
             // Access probabilities
             for (StandardSymbol symbol : standardSymbolList) {
                 System.out.println("Column: " + symbol.getColumn() + ", Row: " + symbol.getRow());
-//                System.out.println("Symbols: " + symbol.getSymbols());
-
                 Map<String, Integer> symbolMap =symbol.getSymbols();
 
                 Random random = new Random();
@@ -61,8 +55,6 @@ public class Main {
                 // Sum all values in the map
                 totalWeight = symbolMap.values().stream().mapToInt(Integer::intValue).sum();
                 int randomValue = random.nextInt(totalWeight - 1); // Generates 0 to (totalWeight - 1)
-
-//                System.out.println("totalWeight: " + totalWeight);
                 System.out.println("randomValue: " + randomValue);
 
                 int currentSum = 0;
@@ -74,17 +66,15 @@ public class Main {
                     if (currentSum > randomValue) {
                       cell = entry.getKey();
                       ansList.add(cell);
-                      finalMetrix[symbol.getRow()][symbol.getColumn()] = cell;
+                        standardSymbolMetrix[symbol.getRow()][symbol.getColumn()] = cell;
                       break;
                     }
-
                 }
-//                System.out.println("ansList  " + ansList);
                 System.out.println("########");
             }
 
             // Print the 2D array
-            for (String[] row : finalMetrix) {
+            for (String[] row : standardSymbolMetrix) {
                 for (String col : row) {
                     System.out.print(col + " ");
                 }
@@ -94,9 +84,6 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
     }
 
 
